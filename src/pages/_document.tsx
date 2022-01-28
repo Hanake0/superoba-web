702238d5b1832ec104e4849185ae59109f4140b4
Hanake0 <bghaesee@outlook.com>
2022-01-28 18:50:42 -0300

    feat(pixel): added meta pixel code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,9 @@ import theme from '../../theme';
 
 class MyDocument extends Document<DocumentProps> {
 	render(): JSX.Element {
+		// @ts-ignore
+		// @ts-ignore
+		// @ts-ignore
 		return (
 			<Html lang="en">
 				<Head>
@@ -29,6 +32,39 @@ class MyDocument extends Document<DocumentProps> {
 						href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
 						rel="stylesheet"
 					/>
+
+					{/* Meta Pixel Code */}
+					<script>{`
+						!function(f, b, e, v, n, t, s) {
+							if (f.fbq) return;
+							n = f.fbq = function() {
+								n.callMethod ?
+									n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+							};
+							if (!f._fbq) f._fbq = n;
+							n.push = n;
+							n.loaded = !0;
+							n.version = '2.0';
+							n.queue = [];
+							t = b.createElement(e);
+							t.async = !0;
+							t.src = v;
+							s = b.getElementsByTagName(e)[0];
+							s.parentNode.insertBefore(t, s)
+						}(window, document, 'script',
+							'https://connect.facebook.net/en_US/fbevents.js');
+						fbq('init', '2083138725178222');
+						fbq('track', 'PageView');
+					`}</script>
+					<noscript>
+						<img
+							height="1"
+							width="1"
+							style={{ display: 'none' }}
+							src="https://www.facebook.com/tr?id=2083138725178222&ev=PageView&noscript=1"
+						/>
+					</noscript>
+					{/* End Meta Pixel Code */}
 				</Head>
 				<body>
 					<ColorModeScript initialColorMode={theme.config.initialColorMode} />
